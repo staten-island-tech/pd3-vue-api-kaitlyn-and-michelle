b1af017254cae3c47a0bbe62ee1f0116ceac0dca
kaitlyndeng <kaitlyndeng20@gmail.com>
2023-04-19 10:14:34 -0400

    Commit 1

---
+++ opendata/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const squirrels = ref('')
+async function getSquirrels() {
+  let res = await fetch()
+}
+</script>
+
+<style lang="scss" scoped></style>
