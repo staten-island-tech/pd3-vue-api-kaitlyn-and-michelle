187e98a711989cf5a5e573da516e2333d97dbffa
kaitlyndeng <kaitlyndeng20@gmail.com>
2023-04-25 10:10:54 -0400

    Chart Links

--- opendata/src/App.vue
+++ opendata/src/App.vue
@@ -1,23 +1,16 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="wrapper">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/chart1">Chart 1</RouterLink>
+      <RouterLink to="/chart2">Chart 2</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
---
+++ opendata/src/views/SquirrelCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card">
+    <h2>{{ squirrel }}</h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
