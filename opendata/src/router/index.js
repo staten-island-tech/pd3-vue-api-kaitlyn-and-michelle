671b68625335ec01d9fa3a5545117a41e1a92dae
MichelleO3O <mchao9406@gmail.com>
2023-05-01 02:18:18 -0400

    made doughnut chart

--- opendata/src/router/index.js
+++ opendata/src/router/index.js
@@ -9,13 +9,14 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/chart2',
+      name: 'Chart2',
+      // // route level code-splitting
+      // // this generates a separate chunk (About.[hash].js) for this route
+      // // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Chart2.vue')
     }
   ]
 })
